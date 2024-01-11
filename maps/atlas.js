82f8d42b446506d2627c663b8c2f0a842bb8fc8c
JesusParedes25 <jeparedes25@gmail.com>
2024-01-11 16:57:53 -0600

    se han agregado mas capas al Atlas

--- maps/atlas.js
+++ maps/atlas.js
@@ -55,10 +55,25 @@ function getColor(d) {
            d > 6.7  ? '#a1dab4' :
                       '#ffffcc';
 }
+// Función para obtener el color en base al número 'nueva_num'
+function getColor2(numero) {
+    return coloresPorNumero[numero] || 'gray'; // Devuelve un color gris como predeterminado si no se encuentra el número
+}
+//colores de las regiones
+var coloresPorNumero = {
+    1: '#CCB974', // Color original que proporcionaste
+    2: '#235B4E', // Color original que proporcionaste
+    3: '#DDC9A3', // Color original que proporcionaste
+    4: '#8172B2', // Color original que proporcionaste
+    5: '#C44E52', // Rojo rosado
+    6: '#4C72B0', // Azul medio
+    7: '#55A868', // Verde claro
+    8: '#691C32', // Lavanda claro
+    9: '#9F2241', // Amarillo tostado
+    10: '#64B5CD', // Azul cielo
+    11: '#FABE7B'  // Coral claro
+};
 
-	  
-
-	 
 	  
 //INFORMACIÓN GENERAL
 	  // Capa Municipios
@@ -92,7 +107,41 @@ function getColor(d) {
 	  }).addTo(map);
 
       
-	  
+      var regionesLayer = L.geoJSON(regiones, {
+        style: function(feature) {
+            // Estilo basado en la propiedad 'nueva_num'
+            return {
+                fillColor: getColor2(feature.properties.nueva_num),
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                fillOpacity: 1
+            };
+        },
+        onEachFeature: function(feature, layer) {
+            layer.on('click', function() {
+                // Construye el contenido HTML para el popup
+                var popupContent = '<table style="width:100%;">' +
+                                   '<tr><td><strong>Nombre del municipio:</strong></td><td>' + feature.properties.NOM_MUN + '</td></tr>' +
+                                   '<tr><td><strong>Número de región:</strong></td><td>' + feature.properties.nueva_num + '</td></tr>' +
+                                   '</table>';
+    
+                // Muestra el popup con SweetAlert2
+                Swal.fire({
+                    title: '<strong>Información de la Región</strong>',
+                    html: popupContent,
+                    width: 600,
+                    background: '#FAFAFA',
+                    customClass: {
+                        title: 'my-title-class',
+                        confirmButton: 'my-confirm-button-class'
+                    },
+                });
+            });
+        }
+    });
+    
+
 	//Capa localidades
 // Crear el grupo de clusters
 var markersCluster = L.markerClusterGroup();
@@ -345,7 +394,37 @@ var cuerposAguaLayer = L.geoJSON(cuerpos_agua, {
         });
     }
 });
+var anpLayer = L.geoJSON(anp, {
+    style: function(feature) {
+        return {
+            color: 'green', // Color de línea
+            fillColor: 'green', // Color de relleno
+            fillOpacity: 0.5, // Opacidad del relleno
+            weight: 1 // Grosor de la línea
+        };
+    },
+    onEachFeature: function(feature, layer) {
+        layer.on('click', function() {
+            var props = feature.properties;
+            var popupContent = '<table style="width:100%;">' +
+                               '<tr><td><strong>Nombre:</strong></td><td>' + props.ANP_NOM + '</td></tr>' +
+                               '<tr><td><strong>Jurisdicción:</strong></td><td>' + props.ANP_TIP + '</td></tr>'
+                               '</table>';
 
+            // Muestra el popup con SweetAlert2
+            Swal.fire({
+                title: '<strong>Información de Área Natural Protegida</strong>',
+                html: popupContent,
+                width: 600,
+                background: '#FAFAFA',
+                customClass: {
+                    title: 'my-title-class',
+                    confirmButton: 'my-confirm-button-class'
+                },
+            });
+        });
+    }
+});
 var corrientesLayer = L.geoJSON(corrientes, {
     style: function(feature) {
         // Estilo definido anteriormente
@@ -403,7 +482,7 @@ var acuiferosLayer = L.geoJSON(acuiferos, {
     }
 });
 
-// Población y vivienda
+//POBLACION Y VIVIENDA
 var PobIndHog = L.geoJSON(pob_ind_hog, {
     style: estiloCoropleta,
     onEachFeature: function (feature, layer) {
@@ -426,6 +505,111 @@ var PobIndHog = L.geoJSON(pob_ind_hog, {
         });
     }
 });
+
+//INDUSTRIA
+var industriaLayer = L.geoJSON(industriales, {
+    style: function(feature) {
+        // Estilo definido anteriormente
+        return {
+            color: 'white',
+            fillColor: '#FF8C00',
+            fillOpacity: 0.8,
+            weight: 1
+        };
+    },
+    onEachFeature: function(feature, layer) {
+        layer.on('click', function() {
+            // Aquí se mantiene la lógica existente para mostrar el nombre del acuífero
+            var popupContent = "<p><b>Nombre: </b> " + feature.properties.Name+ "</p>";
+
+            Swal.fire({
+                title: '<strong>Zonas industriales</strong>',
+                html: popupContent,
+                width: 600,
+                background: '#FAFAFA',
+                customClass: {
+                    title: 'my-title-class',
+                    confirmButton: 'my-confirm-button-class'
+                },
+            });
+        });
+    }
+});
+
+function getColorForActivity(nombre_act) {
+    switch (nombre_act) {
+        case 'Minería de mármol': return '#FF0000'; 
+        case 'Minería de caolín': return '#00FF00'; 
+        case 'Minería de piedra caliza': return 'white '; 
+        case 'Minería de arena y grava para la construcción': return 'green'; 
+        case 'Minería de otras arcillas y de otros minerales refractarios': return 'blue'; 
+        case 'Minería de otras piedras dimensionadas': return 'gray'; 
+        case 'Minería de tezontle y tepetate': return 'yellow'; 
+        case 'Minería de sílice': return 'purple'; 
+        
+
+        default: return '#000000'; // Color por defecto si no coincide con ningún caso
+    }
+}
+var minNoMetalicaLayer = L.geoJSON(minNoMetalica, {
+    pointToLayer: function(feature, latlng) {
+        var markerStyle = {
+            radius: 5,
+            fillColor: getColorForActivity(feature.properties.nombre_act),
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+        return L.circleMarker(latlng, markerStyle);
+    },
+    onEachFeature: function(feature, layer) {
+        layer.on('click', function() {
+            // Construye el contenido HTML para el popup
+            var popupContent = '<table style="width:100%;">' +
+                               '<tr><td><strong>Nombre de actividad:</strong></td><td>' + feature.properties.nombre_act + '</td></tr>' +
+                               '<tr><td><strong>Nombre del establecimiento:</strong></td><td>' + feature.properties.nom_estab + '</td></tr>' +
+                               '<tr><td><strong>Municipio:</strong></td><td>' + feature.properties.municipio + '</td></tr>' +
+                               '<tr><td><strong>Localidad:</strong></td><td>' + feature.properties.localidad + '</td></tr>' +
+                               '<tr><td><strong>Fecha de alta:</strong></td><td>' + feature.properties.fecha_alta + '</td></tr>' +
+                               '</table>';
+
+            // Muestra el popup con SweetAlert2
+            Swal.fire({
+                title: '<strong>Información de la mina</strong>',
+                html: popupContent,
+                width: 600,
+                background: '#FAFAFA',
+                customClass: {
+                    title: 'my-title-class',
+                    confirmButton: 'my-confirm-button-class'
+                },
+            });
+        });
+    }
+});
+
+// Actualización de la leyenda para usar círculos
+var legendMinNoMetalica = L.control({ position: 'bottomleft' });
+legendMinNoMetalica.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    var activities = [
+        'Minería de mármol', 'Minería de caolín', 'Minería de piedra caliza',
+        'Minería de arena y grava para la construcción',
+        'Minería de otras arcillas y de otros minerales refractarios',
+        'Minería de otras piedras dimensionadas', 'Minería de tezontle y tepetate',
+        'Minería de sílice'
+    ];
+    
+    // Crear un div para cada actividad, con un círculo y el texto correspondiente
+    for (var i = 0; i < activities.length; i++) {
+        div.innerHTML += '<div><svg width="18" height="18"><circle cx="9" cy="9" r="8" fill="' + getColorForActivity(activities[i]) + '" /></svg> ' + activities[i] + '</div>';
+    }
+    return div;
+};
+
+
+
 
 
 	  //Lista desplegable
@@ -488,6 +672,7 @@ var overlays = [
 	  expanded: false,
 	  layers: {
 		"Municipios": geojsonLayer,
+        "Regiones administrativas": regionesLayer,
 		"Localidades": markersCluster,
 		"Localidades urbanas": loc_urb,
 		"Agebs": iter_ageb,
@@ -509,7 +694,8 @@ var overlays = [
 		layers: {
 		  "Cuerpos de agua": cuerposAguaLayer,
 		  "Corrientes de agua": corrientesLayer,
-		  "Acuíferos": acuiferosLayer
+		  "Acuíferos": acuiferosLayer,
+          "Áreas Naturales Protegidas": anpLayer
 		}
 	  },
       {
@@ -517,6 +703,14 @@ var overlays = [
 		expanded: false,
 		layers: {
 		  "Población indígena en hogares": PobIndHog
+		}
+	  },
+      {
+		groupName: "Industria",  
+		expanded: false,
+		layers: {
+		  "Zonas industriales": industriaLayer,
+          "Minería no metálica": minNoMetalicaLayer
 		}
 	  },
 ];
@@ -544,6 +738,24 @@ legendMunicipios.onAdd = function(map) {
     div.innerHTML = '<i style="background: #9f2241"></i> Municipios<br>';
     return div;
 };
+// Crear la leyenda para la capa de regiones
+// Crear la leyenda para la capa de regiones
+var legendRegiones = L.control({ position: 'bottomright' });
+legendRegiones.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = '<strong>Regiones</strong><br>'; // Título de la leyenda
+
+    // Iterar sobre la paleta de colores y crear un elemento en la leyenda para cada uno
+    for (var i = 1; i <= 12; i++) {
+        div.innerHTML +=
+            '<div class="legend-item"><i style="background:' + getColor2(i) + '; width: 18px; height: 18px; margin-right: 5px; vertical-align: middle;"></i> ' +
+            'Región ' + i + '</div>';
+    }
+
+    return div;
+};
+
+// Asegúrate de agregar los estilos necesarios para .legend-item en tu CSS
 
 var legendLocalidades = L.control({position: 'bottomleft'});
 legendLocalidades.onAdd = function(map) {
@@ -569,6 +781,12 @@ var legendMunicipal = L.control({position: 'bottomleft'});
 legendMunicipal.onAdd = function(map) {
     var div = L.DomUtil.create('div', 'info legend');
     div.innerHTML = '<i style="background: url(../images/iconos/municipal.png) no-repeat center; background-size: contain;"></i> Red vial Municipal<br>';
+    return div;
+};
+var legendANP = L.control({ position: 'bottomleft' });
+legendANP.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML += '<i style="background: green; width: 18px; height: 18px; float: left; margin-right: 8px;"></i> Áreas Naturales Protegidas<br>';
     return div;
 };
 var legendPobInd = L.control({ position: 'bottomleft' });
@@ -603,7 +821,12 @@ legendPobInd.onAdd = function (map) {
     return div;
 };
 
-legendPobInd.addTo(map);
+var legendCorrientes = L.control({position: 'bottomleft'});
+legendCorrientes.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = '<i style="background: url(../images/iconos/corrientes.png) no-repeat center; background-size: contain;"></i> Corrientes de agua<br>';
+    return div;
+};
 
 
 
@@ -616,7 +839,10 @@ function updateLegends() {
     var estatalVisible = map.hasLayer(estatalLayer);
     var municipalVisible = map.hasLayer(municipalLayer);
     var PobIndHogVisible=map.hasLayer(PobIndHog);
-
+    var minNoMetalicaVisible = map.hasLayer(minNoMetalicaLayer);
+    var corrientesVisible=map.hasLayer(corrientesLayer);
+    var anpVisible = map.hasLayer(anpLayer);
+    
     if (municipiosVisible) {
         legendMunicipios.addTo(map);
     } else {
@@ -651,9 +877,29 @@ function updateLegends() {
     } else {
         map.removeControl(legendPobInd);
     }
-    
+    if (minNoMetalicaVisible) {
+        legendMinNoMetalica.addTo(map);
+    } else {
+        map.removeControl(legendMinNoMetalica);
+    }
+    if (corrientesVisible) {
+        legendCorrientes.addTo(map);
+    } else {
+        map.removeControl(legendCorrientes);
+    }
+    if (anpVisible) {
+        legendANP.addTo(map);
+    } else {
+        map.removeControl(legendANP);
+    }
+    if (map.hasLayer(regionesLayer)) {
+        legendRegiones.addTo(map);
+    } else if (map._container.contains(legendRegiones._container)) {
+        // Asegúrate de que la leyenda existe antes de intentar eliminarla
+        map.removeControl(legendRegiones);
+    }
 }
-
+map.on('overlayadd overlayremove', updateLegends);
 
 // Actualizar las leyendas al cargar el mapa
 updateLegends();
